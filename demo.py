934b4c23ff8c66422afd7e010d758e743ec01f74
anushka-u <anushkau032@gmail.com>
2024-04-20 16:09:52 +0530

    PythonPractice Demo

---
+++ demo.py
@@ -0,0 +1,27 @@
+# print(2)
+# print(2*6)
+# print(3.13)
+# print("Hello")
+#
+# print()
+
+
+S = 'a'
+print(S)
+print(type(S))
+
+a = 8.796543
+print(a)
+print(type(a))
+
+i = 2
+print(i)
+print(type(i))
+
+b = True
+print(b)
+print(type(b))
+
+
+
+
